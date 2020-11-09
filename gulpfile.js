6f00c860fa6bfa12e1141bc1fd229b9b0868a7a1
javizzyv <javi.ventus@hotmail.com>
2020-11-09 22:29:58 +0100

    Cambio target install #18

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,17 @@
 const gulp = require('gulp');
 const mocha = require('gulp-mocha');
-//const run = require('gulp-run');
-const gulpI = require('gulp-shell');
+const run = require('gulp-run');
+
 
 gulp.task('test', async function() {
     gulp.src('test/test.js')
     .pipe(mocha());
 });
 
-
-gulp.task('install', gulpI.task(['npm install']));
-
-
-/*gulp.task('install', async function() {
+gulp.task('install', async function() {
     gulp.src('./')
         .pipe(run('npm install'));
-})*/
+})
 
 exports.default = function(cb) {
     cb();
