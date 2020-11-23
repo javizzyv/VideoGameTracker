120ff221db1dc7ea476f18c81e5b4cb2fec3e094
javizzyv <javi.ventus@hotmail.com>
2020-11-23 22:52:53 +0100

    Añado lo basico de Netlify #21

---
+++ netlify/index.js
@@ -0,0 +1,18 @@
+const datos = require ('./datos')
+
+exports.handler = async event => {
+
+    var vgs = '';
+
+    for (vg in datos.datos.videojuegos){
+        if(datos.datos.videojuegos[vg].genero == "RPG"){
+            vgs += datos.datos.videojuegos[vg];
+        }
+    }
+  
+    return {
+      statusCode: 200,
+      body: vgs.toString(),
+    }
+  
+  }
