a1f81a4ea38d0c1c7a2a3e73d4adc7c5a08cd0a4
javizzyv <javi.ventus@hotmail.com>
2020-11-23 19:53:26 +0100

    Añado funcion en vercel para ver el favorito de videojuegos predeterminados #19

--- api/index.js
+++ api/index.js
@@ -1,11 +1,24 @@
-async function Hola() {
-  return 'Hola';
+var VG = require("./Videojuego");
+var videojuegos = new Array()
+
+async function Favorito() {
+  var vgFav = new VG.Videojuego('Pokemon Platino','RPG por turnos de coleccionar monstruos',gen.mas16.RPG, 9.5, 34);
+  videojuegos = new Array();
+  videojuegos.push(vgFav);
+  vgFav = new VG.Videojuego('Pokemon Esmeralda','RPG por turnos de coleccionar monstruos',gen.mas16.RPG, 9.5, 30);
+  videojuegos.push(vgFav);
+  vgFav = new VG.Videojuego('Pokemon Cristal','RPG por turnos de coleccionar monstruos',gen.mas16.RPG, 9.5, 40);
+  videojuegos.push(vgFav);
+  vgFav = vgt.masLargo(videojuegos);
+  return vgFav.getNombre().toString();
 }
 
 module.exports = async (req, res) => { // this function will be launched when the API is called.
   try {
-    res.send(await Hola()) // send the lyrics
+    status: 200, 
+    res.send(await Favorito()) // send the lyrics
   } catch (err) {
+    status: 500,
     res.send(err) // send the thrown error
   }
 }
