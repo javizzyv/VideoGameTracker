c732562b178d271cfa20554bb8fa22b062365a1e
javizzyv <javi.ventus@hotmail.com>
2020-11-23 20:33:59 +0100

    Fix a la funcion de vercel #19

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
-const datos = require ('datos.json')
+const datos = require ('./datos.json')
 
 async function Favorito() {
-  var notaAux = 0;
+  /*var notaAux = 0;
   var datosJ = JSON.parse(datos);
-  return datosJ['videojuegos'][1]['titulo']
+  return datosJ['videojuegos'][1]['titulo']*/
+  return 'Hola'
 }
 
 module.exports = async (req, res) => { // this function will be launched when the API is called.
