b2f9e694a8051c2a8ad5d06299beb1d9ca24ed43
javizzyv <javi.ventus@hotmail.com>
2020-11-23 20:27:57 +0100

    Añados datos por json #19

--- api/index.js
+++ api/index.js
@@ -1,16 +1,9 @@
-var VG = require("./Videojuego");
-var videojuegos = new Array()
+const datos = require ('datos.json')
 
 async function Favorito() {
-  var vgFav = new VG.Videojuego('Pokemon Platino','RPG por turnos de coleccionar monstruos',gen.mas16.RPG, 9.5, 34);
-  videojuegos = new Array();
-  videojuegos.push(vgFav);
-  vgFav = new VG.Videojuego('Pokemon Esmeralda','RPG por turnos de coleccionar monstruos',gen.mas16.RPG, 9.5, 30);
-  videojuegos.push(vgFav);
-  vgFav = new VG.Videojuego('Pokemon Cristal','RPG por turnos de coleccionar monstruos',gen.mas16.RPG, 9.5, 40);
-  videojuegos.push(vgFav);
-  vgFav = vgt.masLargo(videojuegos);
-  return vgFav.getNombre();
+  var notaAux = 0;
+  var datosJ = JSON.parse(datos);
+  return datosJ['videojuegos'][1]['titulo']
 }
 
 module.exports = async (req, res) => { // this function will be launched when the API is called.
